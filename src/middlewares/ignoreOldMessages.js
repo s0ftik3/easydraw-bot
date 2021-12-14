24feac304a92213a5cbf86a5a67dcca25c1d4a5f
Vyacheslav <slava021323@gmail.com>
2021-12-14 08:10:44 +0600

    0.2.6
    - Proper old messages ignore system

--- src/middlewares/ignoreOldMessages.js
+++ src/middlewares/ignoreOldMessages.js
@@ -3,12 +3,16 @@
 module.exports = () => {
     return async (ctx, next) => {
         try {
-            if (new Date().getTime() / 1000 - (ctx?.message?.date) < (5 * 60)) {
-                return next();
+            if (ctx.updateType === 'message') {
+                if (new Date().getTime() / 1000 - (ctx?.message?.date) < (5 * 60)) {
+                    return next();
+                } else {
+                    console.info(
+                        `[${ctx.chat.id}] Ignoring update (${ctx.updateType})`
+                    );
+                }
             } else {
-                console.info(
-                    `[${ctx.chat.id}] Ignoring update (${ctx.updateType})`
-                );
+                return next();
             }
         } catch (err) {
             console.error(err)
